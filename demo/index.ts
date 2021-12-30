21d71fb51766aaee28292a1fcda4fcd9746db1f6
ardean <aron.stifani@gmail.com>
2021-12-30 01:32:47 +0100

    implement v0.3.0

--- demo/index.ts
+++ demo/index.ts
@@ -2,11 +2,25 @@ import UI from "../src";
 
 (() => {
   const element = document.querySelector<HTMLElement>("jsgbc-ui");
-  new UI(element)
-    .addListener("down", (e) => {
-      console.log("down", e.detail.button);
+  const ui = new UI(element)
+    .addListener("ButtonDown", (buttonName: string) => {
+      console.log("ButtonDown", buttonName);
     })
-    .addListener("up", (e) => {
-      console.log("up", e.detail.button);
+    .addListener("ButtonUp", (buttonName: string) => {
+      console.log("ButtonUp", buttonName);
     });
+
+  ui.setLoading(true);
+
+  setTimeout(() => {
+    ui.setLoading(false);
+
+    setTimeout(() => {
+      ui.setFullscreen(true);
+
+      setTimeout(() => {
+        ui.setFullscreen(false);
+      }, 1000);
+    }, 1000);
+  }, 1000);
 })();
